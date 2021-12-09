7bbcf7b52f558ae5d2abcd9d383fd316717c03cf
besharpe5 <besharpe15@gmail.com>
2021-12-08 20:48:09 -0500

    fixed bug with player name being null

--- assets/js/game.js
+++ assets/js/game.js
@@ -170,6 +170,17 @@ var randomNumber = function(min, max) {
         break;
     }
   };
+
+  //function to set name
+  var getPlayerName = function() {
+      var name = "";
+
+      while (name === "" || name === null) {
+          name = prompt("What is your robot's name?");
+      }
+      console.log("Your robot's name is " + name);
+      return name;
+  };
   
   /* END GAME FUNCTIONS */
   
@@ -177,7 +188,7 @@ var randomNumber = function(min, max) {
   
   // player information
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
